cd16864bdb2bb679320444607a0280f64d0c8dbd
ryislov <46944741+ryislov@users.noreply.github.com>
2019-04-12 13:22:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+	$(".btn-flat").on('click', function() {
+		$(".btn-flat").replaceWith("<p>i'm doing alright :) a little tired, but c'est la vie, ya know?</p>")
+	});
+
+});
